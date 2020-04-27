c9cc01acb6a27e484c38831fc2c6f74e63162782
Mango <781132399@qq.com>
2020-04-28 00:55:40 +0800

    new project and add resources

---
+++ LessonTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LessonTwo
+//
+//  Created by Hao Cui  on 2020/4/28.
+//  Copyright © 2020 Hao Cui . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
