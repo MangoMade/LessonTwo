b915dfdea6a72062ba7af691533670049b77a4d5
Mango <781132399@qq.com>
2020-04-28 12:10:00 +0800

    add setting page

---
+++ LessonTwo/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  LessonTwo
+//
+//  Created by Hao Cui  on 2020/4/28.
+//  Copyright © 2020 Hao Cui . All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
