f07304be87885cd8088c942daf040f24cd91320e
Mango <781132399@qq.com>
2020-04-28 11:01:32 +0800

    handle login button action

--- LessonTwo/LoginViewController.swift
+++ LessonTwo/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
     
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
         loginButton.layer.cornerRadius = 4
     }
 
-
+    @IBAction func handleLoginButton(_ sender: Any) {
+    }
+    
 }
 
